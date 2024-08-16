c2d5e085529540aa76f1e572043fb6efea34ba44
williameclee <williameclee@gmail.com>
2024-08-16 10:36:39 -0700

    Updated gia2plmt to be compatible with the new Steffen models

--- gia2plmt.m
+++ gia2plmt.m
@@ -12,11 +12,30 @@
 %		Returns the GIA geoid changes truncated to degree L.
 %   Plmt = gia2plmt(__, 'Name', Value)
 %   [Plmt, PlmtU, PlmtL] = gia2plmt(__)
-%		Also returns the upper and lower bounds of the GIA model, if available.
+%		Also returns the upper and lower bounds of the GIA model, if
+%       available.
 %
 % Input arguments
 %   model - Name of the GIA model
-%		The default model is 'Paulson07'.
+%       - 'Steffen_ice6g_vm5a': A model computed by H. Steffen using the
+%           ice6g ice model and vm5a viscosity profile. Other models from
+%           this dataset are also available and use the original naming
+%           scheme. For example, 'Steffen_anu-ice_i72'.
+%           This family of models can also be specified as a cell array,
+%           e.g. {'Steffen', 'ice6g', 'vm5a'}.
+%       - 'Paulson07': A model based on the ICE-5G ice load model of
+%           Peltier (2004). Suitable for both Antarctica and Greenland. As
+%           corrected by Geruo A and J. Wahr.
+%           Please avoid using this model for oceans.
+%       - 'Wangetal08': A model based on the older ICE-4G ice model, and
+%           viscosity which varies laterally. Suitable for Greenland.
+%       - 'IJ05_R2': A model based on the Ivins et al. (2013) ice model.
+%           Suitable for Antarctica.
+%       - 'IJ05': A model based on the Ivins and James (2005) ice model.
+%           Suitable for Antarctica.
+%       - 'W12a_v1': A 'best' model from Whitehouse et al. (2012). Suitable
+%           only for Antarctica.
+%		The default model is 'Steffen_ice6g_vm5a'.
 %   days - Number of days to calculate the GIA change for
 %   time - Vector of dates to calculate the GIA change for
 %		The input can be in DATENUM or DATETIME format.
@@ -30,26 +49,51 @@
 % See also
 %   CORRECT4GIA
 %
+% Data source
+%   Paulson, A., S. Zhong, and J. Wahr. (2007). Inference of mantle
+%       viscosity from GRACE and relative sea level data, Geophys. J. Int.
+%       171, 497–508. doi: 10.1111/j.1365-246X.2007.03556.x
+%   Geruo, A., Wahr, J. & Zhong, S. (2013). Computations of the
+%       viscoelastic response of a 3-D compressible Earth to surface
+%       loading: An application to Glacial Isostatic Adjustment in
+%       Antarctica and Canada. Geophys. J. Int. 192, 557-572.
+%   Ivins, E. R., T. S. James, J. Wahr, E. J. O. Schrama, F. W. Landerer,
+%       and K. M. Simon. (2013). Antarctic contribution to sea level rise
+%       observed by GRACE with improved GIA correction, Journal of
+%       Geophysical Research: Solid Earth, vol. 118, 3126-3141, doi:
+%       10.1002/jgrb.50208
+%   Ivins, E. R., T. S. James. (2005). Antarctic glacial isostatic
+%       adjustment: a new assessment, Antarctic Science, vol. 17(4),
+%       541-553,  doi: 10.1017/S0954102005002968
+%   Wang, H., P. Wu, and W. van der Wal. (2018). Using postglacial sea
+%       level, crustal velocities and gravity-rate-of-change to constrain
+%       the influence of thermal effects on mantle lateral heterogeneities,
+%       Journal of Geodynamics 46, 104-117. doi: 10.1016/j.jog.2008.03.003
+%   Whitehouse, P. L., Bentley, M. J., Milne, G. A., King, M. A., Thomas,
+%       I. D. (2012). A new glacial isostatic adjustment model for
+%       Antarctica: calibrated and tested using observations of relative
+%       sea-level change and present-day uplift rates. Geophysical Journal
+%       International 190, 1464-1482. doi:10.1111/j.1365-246X.2012.05557.x
+%   Steffen, H. (2021). Surface Deformations from Glacial Isostatic
+%       Adjustment Models with Laterally Homogeneous, Compressible Earth
+%       Structure (1.0) [Data set]. Zenodo. doi: 10.5281/zenodo.5560862.
+%
 % Last modified by
-%   2024/08/15, williameclee@arizona.edu (@williameclee)
+%   2024/08/16, williameclee@arizona.edu (@williameclee)
 
 function varargout = gia2plmt(varargin)
     %% Initialisation
-    [time, model, L, ~] = parseinputs(varargin{:});
+    [time, model, L, beQuiet] = parseinputs(varargin{:});
 
     %% Loading the model
-    % Where the model save files are kept
-    if strncmp(model, 'Morrow', 6)
-        inputFolder = fullfile(getenv('IFILES'), 'GIA', model(1:6));
-    else
-        inputFolder = fullfile(getenv('IFILES'), 'GIA', model);
+    % Load this data (saved as lmcosiM)
+    inputPath = finddatafile(model);
+    load(inputPath, 'lmcosiM', 'lmcosiU', 'lmcosiL');
+
+    if ~beQuiet
+        fprintf('%s loaded model %s\n', upper(mfilename), inputPath);
     end
 
-    % And the appropriate name
-    inputPath = fullfile(inputFolder, sprintf('%s_SD.mat', model));
-
-    % Load this data (saved as lmcosiM)
-    load(inputPath, 'lmcosiM', 'lmcosiU', 'lmcosiL');
     hasBounds = exist('lmcosiU', 'var') && exist('lmcosiL', 'var');
 
     %% Some additional processing
@@ -102,7 +146,8 @@ function varargout = gia2plmt(varargin)
         GIAtL = nan;
 
         if nargout > 1
-            warning('Upper and lower bounds are not available for this model');
+            warning( ...
+            'Upper and lower bounds are not available for this model');
         end
 
     end
@@ -119,12 +164,12 @@ end
 
 %% Subfunctions
 function varargout = parseinputs(varargin)
-    modelD = 'Paulson07';
+    modelD = 'Steffen_ice6g_vm5a';
     p = inputParser;
+    addOptional(p, 'Model', modelD, ...
+        @(x) ischar(x) || (iscell(x) && length(x) == 3) || isempty(x));
     addOptional(p, 'Time', [], ...
         @(x) isnumeric(x) || isdatetime(x) || isempty(x));
-    addOptional(p, 'Model', modelD, ...
-        @(x) ischar(x) || isempty(x));
     addOptional(p, 'L', [], ...
         @(x) isnumeric(x) || isempty(x));
     addParameter(p, 'BeQuiet', false, @islogical);
@@ -139,6 +184,18 @@ function varargout = parseinputs(varargin)
         time = datenum(time); %#ok<DATNM>
     end
 
+    if iscell(model)
+
+        if ~strcmpi(model{1}, 'Steffen')
+            error('Unrecognised model name %s', upper(model{1}));
+        elseif ~ismember(model{2}, {'anu-ice', 'ice6g', 'ice7g'})
+            error('Unrecognised ice model %s', upper(model{2}));
+        end
+
+        model = sprintf('%s_%s_%s', model{1}, model{2}, model{3});
+
+    end
+
     varargout = {time, model, L, beQuiet};
 
 end
@@ -146,8 +203,10 @@ end
 function plmt = plm2plmt(plm, deltaYear)
     plmt = zeros([length(deltaYear), size(plm)]);
 
-    plmt(:, :, 1:2) = repmat(reshape(plm(:, 1:2), [1, length(plm), 2]), [length(deltaYear), 1, 1]);
-    plmt(:, :, 3:4) = deltaYear(:) .* reshape(plm(:, 3:4), [1, length(plm), 2]);
+    plmt(:, :, 1:2) = repmat(reshape( ...
+        plm(:, 1:2), [1, length(plm), 2]), [length(deltaYear), 1, 1]);
+    plmt(:, :, 3:4) = ...
+        deltaYear(:) .* reshape(plm(:, 3:4), [1, length(plm), 2]);
     plmt = squeeze(plmt);
 
 end
@@ -171,15 +230,54 @@ function plotgiamap(GIAt, time, deltaYear, model)
     GIAmesh = max(min(GIAmesh, cLim(2)), cLim(1));
 
     figure(999)
-    set(gcf, "Name", sprintf('GIA change (%s)', upper(mfilename)), "NumberTitle", "off")
+    % Protect underscore in model name
+    model = strrep(model, '_', '\_');
+    set(gcf, "Name", sprintf('GIA change (%s)', upper(mfilename)), ...
+        "NumberTitle", "off")
     clf
 
     title(sprintf('Model: %s', model))
 
-    [~, cLevels] = loadcbar(cLim, cStep, "Title", 'GIA change [kg/m^2/yr]', "Colormap", 'temperature anomaly');
+    [~, cLevels] = loadcbar(cLim, cStep, ...
+        "Title", 'GIA change [kg/m^2/yr]', ...
+        "Colormap", 'temperature anomaly');
 
     hold on
     contourf(lon, lat, GIAmesh, cLevels, "LineStyle", 'none');
     plotqdm(coastLonlat, 'k');
     hold off
+end
+
+function inputPath = finddatafile(model)
+
+    if exist(model, 'file') == 2
+        inputFolder = fullfile(fileparts(mfilename('fullpath')), 'models');
+    else
+
+        if ~isempty(getenv('GIA'))
+            inputFolder = getenv('GIA');
+        elseif ~isempty(getenv('IFILES'))
+            inputFolder = fullfile(getenv('IFILES'), 'GIA');
+        else
+            error('GIA folder not found')
+        end
+
+        if strncmp(model, 'Morrow', 6)
+            inputFolder = fullfile(inputFolder, model(1:6));
+        elseif strncmp(model, 'Steffen', 7)
+            inputFolder = fullfile(inputFolder, 'SteffenGrids');
+        else
+            inputFolder = fullfile(inputFolder, model);
+        end
+
+    end
+
+    % And the appropriate name
+    inputPath = fullfile(inputFolder, sprintf('%s_SD.mat', model));
+
+    if exist(inputPath, 'file') ~= 2
+        error('Model %s not found\nIt should be kept at %s', ...
+            upper(model), inputPath);
+    end
+
 end
